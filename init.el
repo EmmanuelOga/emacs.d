efce4eed82261b11d504d4e51a6a4f6d7bfba215
Emmanuel Oga <contact@emmanueloga.com>
2020-05-15 05:25:06 -0700

    ok

--- init.el
+++ init.el
@@ -1,3 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(custom-enabled-themes '(zerodark))
+ '(custom-safe-themes
+   '("846ef3695c42d50347884515f98cc359a7a61b82a8d0c168df0f688cf54bf089" default))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -13,14 +32,42 @@
 
 (straight-use-package 'use-package)
 
-(use-package treemacs
- :straight t)
+(use-package treemacs :straight t)
+(use-package treemacs-evil :straight t)
+(use-package evil :straight t)
+(use-package no-littering :straight t)
+(use-package cider :straight t)
+(use-package zerodark-theme :straight t)
 
-(use-package treemacs-evil
- :straight t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package evil
- :straight t)
+(setq user-full-name "Emmanuel Oga"
+      user-mail-address "EmmanuelOga@gmail.com")
 
-(use-package no-littering
- :straight t)
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
+;; reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; warn when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+(toggle-scroll-bar -1) 
+(tool-bar-mode -1) 
+(blink-cursor-mode -1)
+(setq ring-bell-function 'ignore)
+(setq inhibit-startup-screen t)
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+(fset 'yes-or-no-p 'y-or-n-p)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8-unix)
+(setq default-buffer-file-coding-system 'utf-8-unix)
