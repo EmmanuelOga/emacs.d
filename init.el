663ab36a7a40b04c4e641050d03886c7a3e9f2fa
Emmanuel Oga <contact@emmanueloga.com>
2020-05-15 04:03:11 -0700

    initial commit

---
+++ init.el
@@ -0,0 +1,26 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(use-package treemacs
+ :straight t)
+
+(use-package treemacs-evil
+ :straight t)
+
+(use-package evil
+ :straight t)
+
+(use-package no-littering
+ :straight t)
