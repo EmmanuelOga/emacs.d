32a3980984db714d9b283e880e323671fab97b38
Emmanuel Oga <contact@emmanueloga.com>
2020-05-15 21:24:23 -0700

    ok

--- init.el
+++ init.el
@@ -1,22 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(column-number-mode t)
- '(custom-enabled-themes '(zerodark))
- '(custom-safe-themes
-   '("846ef3695c42d50347884515f98cc359a7a61b82a8d0c168df0f688cf54bf089" default))
- '(size-indication-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
-
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -41,8 +22,9 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq user-full-name "Emmanuel Oga"
-      user-mail-address "EmmanuelOga@gmail.com")
+(setq
+ user-full-name "Emmanuel Oga"
+ user-mail-address "EmmanuelOga@gmail.com")
 
 ;; Always load newest byte code
 (setq load-prefer-newer t)
@@ -71,3 +53,22 @@
 (set-terminal-coding-system 'utf-8-unix)
 (set-keyboard-coding-system 'utf-8-unix)
 (setq default-buffer-file-coding-system 'utf-8-unix)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(custom-enabled-themes '(zerodark))
+ '(custom-safe-themes
+   '("846ef3695c42d50347884515f98cc359a7a61b82a8d0c168df0f688cf54bf089" "48c8e318a70466bb6d46509c8d6d458c047ee8107541b8de973cb3cb048b9592" default))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
